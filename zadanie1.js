0422d793e78de27b2a5b9a8c4f9e290d1155546f
magdalenakruk <57995043+magdalenakruk@users.noreply.github.com>
2019-11-26 13:11:48 +0100

    Add files via upload

--- zadanie1.js
+++ zadanie1.js
@@ -1,1 +1,26 @@
+const CITY_SPEED_LIMIT = 50;
+const TWO_LANE_EXPRESSWAY_SPEED_LIMIT = 120;
+const HIGHWAY_SPEED_LIMIT = 140;
+let speed = 49;
+let typeOfRoad = 'city';
 
+if (typeOfRoad === 'city' && speed<CITY_SPEED_LIMIT) {
+    console.log('Miasto: jedziesz prawidlowo')
+} else if (speed>CITY_SPEED_LIMIT) {
+    console.log('Miasto: przekroczyles prędkość')
+}
+
+if (typeOfRoad === 'expressway' && speed<TWO_LANE_EXPRESSWAY_SPEED_LIMIT) {
+    console.log('Eska: jedziesz prawidlowo')
+} else if (speed>TWO_LANE_EXPRESSWAY_SPEED_LIMIT) {
+    console.log('Eska: przekroczyles prędkość')
+}
+
+switch (true) {
+    case typeOfRoad === 'highway' && speed<HIGHWAY_SPEED_LIMIT:
+        console.log('Autostrada: jedziesz prawidlowo');
+        break;
+    case typeOfRoad === 'highway' && speed>HIGHWAY_SPEED_LIMIT:
+        console.log('autostrada: przekroczyles prędkość')
+        break;
+}
---
+++ zadanie2.js
@@ -0,0 +1,3 @@
+for (let i = 5, max = 12; i <= max; i += 1) {
+    console.log(i);
+  }
